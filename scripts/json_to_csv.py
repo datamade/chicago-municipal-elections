507f5556984fd72105b56a201fa69e420bc24c7e
Forest Gregg <fgregg@uchicago.edu>
2018-04-04 09:14:02 -0500

    registered voters and cast ballots first in csv

--- scripts/json_to_csv.py
+++ scripts/json_to_csv.py
@@ -9,8 +9,10 @@ field_names = list(features[0]['properties'].keys())
 
 field_names.remove('WARD')
 field_names.remove('PRECINCT')
+field_names.remove('REGISTERED VOTERS')
+field_names.remove('BALLOTS CAST')
 
-field_names = ['WARD', 'PRECINCT'] + field_names
+field_names = ['WARD', 'PRECINCT', 'REGISTERED VOTERS', 'BALLOTS CAST'] + field_names
 
 writer = csv.DictWriter(sys.stdout, fieldnames=field_names, dialect='unix')
 writer.writeheader()
